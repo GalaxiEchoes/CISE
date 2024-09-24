0b5547d8f6519c23f69b476257a8796912cc9812
pendevx <pendevx@gmail.com>
2024-09-24 16:03:14 +1200

    MVP-003: search working

--- frontend/src/app/search/page.tsx
+++ frontend/src/app/search/page.tsx
@@ -12,7 +12,6 @@ export default function SearchPage() {
 
     async function search(e: React.FormEvent) {
         e.preventDefault();
-        const searchPhrase = "a";
         const res = await searchArticles(searchPhrase);
         setSearchResults(res);
     }
@@ -20,12 +19,19 @@ export default function SearchPage() {
     return (
         <div className="px-10">
             <form className="flex gap-2 pt-4" onSubmit={search}>
-                <input type="text" onInput={e => setSearchPhrase(e.currentTarget.value)} className="w-full block border-2 border-solid border-black text-xl px-2 py-1 grow" placeholder="Type your search phrase here..." />
+                <input
+                    type="text"
+                    onInput={(e) => setSearchPhrase(e.currentTarget.value)}
+                    className="block w-full grow border-2 border-solid border-black px-2 py-1 text-xl"
+                    placeholder="Type your search phrase here..."
+                />
                 <Button type="submit">Submit</Button>
             </form>
 
             <div>
-                {searchResults.map((article: Article, index: number) => <ArticleCard key={article._id} article={article} />)}
+                {searchResults.map((article: Article, index: number) => (
+                    <ArticleCard key={article._id} article={article} />
+                ))}
             </div>
         </div>
     );
--- frontend/src/lib/api/search.ts
+++ frontend/src/lib/api/search.ts
@@ -2,11 +2,11 @@ import { Article } from "@/models/Articles";
 import { apiGet } from "../Api";
 
 export async function searchArticles(searchPhrase: string): Promise<Article[]> {
-    const res = await apiGet(`/api/Article/search?phrase=${searchPhrase}`);
+    const res = await apiGet(`/api/Article/search?query=${searchPhrase}`);
 
     if (res != null) {
         return res.json() as Promise<Article[]>;
     }
-    
+
     return [];
 }
