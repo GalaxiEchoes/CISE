ef5a7fe3932e0ab38a6e3baeee9a91f614b4cbf1
coriandar <88608040+coriandar@users.noreply.github.com>
2024-09-25 19:01:10 +1200

    [fix]: add package-lock.json

--- backend/src/auth/auth.decorator.ts
+++ backend/src/auth/auth.decorator.ts
@@ -1,5 +1,5 @@
 import { applyDecorators, UseGuards, SetMetadata } from "@nestjs/common";
-import { AuthGuard } from "./auth.guard.ts";
+import { AuthGuard } from "./auth.guard";
 
 export function Auth(...permissions: string[]) {
     return applyDecorators(
