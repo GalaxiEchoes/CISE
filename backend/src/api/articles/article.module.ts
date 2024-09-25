49d1fcc0c1563ff771054d9addd1b66f6abb80d4
pendevx <pendevx@gmail.com>
2024-09-25 19:26:38 +1200

    firebase admin route

--- backend/src/api/articles/article.module.ts
+++ backend/src/api/articles/article.module.ts
@@ -3,7 +3,7 @@ import { ArticleController } from "./article.controller";
 import { ArticleService } from "./article.service";
 import { MongooseModule } from "@nestjs/mongoose";
 import { Article, ArticleSchema } from "./article.schema";
-import { FirebaseAdmin } from "src/config/firebase.setup";
+import { FirebaseAdmin } from "../../config/firebase.setup";
 
 @Module({
     imports: [
